5b47f48f8278aced788f001529dbb9bb6d671a40
believer <rickard.laurin@gmail.com>
2017-10-23 19:10:26 +0200

    Add Relay setup

---
+++ src/createRelayEnvironment.js
@@ -0,0 +1,27 @@
+const { Environment, Network, RecordSource, Store } = require('relay-runtime')
+
+function fetchQuery (operation, variables) {
+  return fetch('http://graphql.willcodefor.beer/graphql', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  })
+}
+
+const network = Network.create(fetchQuery)
+
+const source = new RecordSource()
+const store = new Store(source)
+
+export default new Environment({
+  network,
+  store,
+})
