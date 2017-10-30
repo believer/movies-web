ea927f545ef783a84ef989b3f39eb7aa04edbb8c
believer <rickard.laurin@gmail.com>
2017-10-30 19:22:10 +0100

    Handle undefined and expired token

--- src/environment/ApolloSetup.js
+++ src/environment/ApolloSetup.js
@@ -3,13 +3,51 @@ import { ApolloLink } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+function deconstructJWT (token) {
+  const segments = token.split('.')
+
+  if (!segments instanceof Array || segments.length !== 3) {
+    throw new Error('Invalid JWT')
+  }
+
+  return JSON.parse(decodeURIComponent(escape(window.atob(segments[1]))))
+}
+
 const httpLink = createHttpLink({ uri: process.env.REACT_APP_API_URL })
+
 const middlewareLink = new ApolloLink((operation, forward) => {
+  const unprotectedRoutes = ['login', 'register']
+
+  if (unprotectedRoutes.includes(operation.operationName)) {
+    return forward(operation)
+  }
+
+  const token = localStorage.getItem('token')
+
+  // Redirect to login if no token
+  if (!token) {
+    window.location.href = '/'
+
+    return forward(operation)
+  }
+
+  // Check if token has expired
+  const tokenExpires = deconstructJWT(token).exp
+
+  if (Date.now() - tokenExpires * 1000 > 0) {
+    localStorage.removeItem('token')
+    window.location.href = '/'
+
+    return forward(operation)
+  }
+
+  // Set token on all requests
   operation.setContext({
     headers: {
-      authorization: localStorage.getItem('token') || null,
+      authorization: `Bearer ${token}` || null,
     },
   })
+
   return forward(operation)
 })
 
