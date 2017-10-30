f807b93439181192d15d3c7a92d85c9fdc4ad309
Rickard Laurin <rickard.laurin@gmail.com>
2017-10-30 12:02:24 +0100

    Adjust Movie content styling

--- src/views/LoggedIn/Movie/MovieOverview.js
+++ src/views/LoggedIn/Movie/MovieOverview.js
@@ -4,6 +4,7 @@ const MovieOverview = styled.p`
   color: #666;
   font-size: 14px;
   line-height: 1.5em;
+  margin-bottom: 40px;
   max-width: 50%;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.md}px) {
