fa197f8071a9d201edcfb8e15e51b7583c1ea7d8
Rickard Laurin <rickard.laurin@gmail.com>
2017-11-06 11:59:12 +0100

    Actually add stuff

---
+++ src/views/LoggedIn/Feed/FeedToplist.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const FeedToplist = styled.div`font-size: 14px;`
+
+export const FeedTopListItem = styled.div`margin-bottom: 10px;`
+export const FeedToplistRating = styled.div`
+  color: #999;
+  margin-bottom: 10px;
+`
+
+export default FeedToplist
---
+++ src/views/LoggedIn/Feed/FeedWrap.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const FeedWrap = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 200px;
+  grid-column-gap: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1140px;
+`
+
+export default FeedWrap
